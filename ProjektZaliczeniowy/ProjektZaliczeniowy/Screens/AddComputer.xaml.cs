68659742f330bb8468818dce109b0c88b41a3063
Mateusz Ożóg <mateusz2345678@gmail.com>
2021-02-01 20:45:36 +0100

    :+1: New computer form

--- ProjektZaliczeniowy/ProjektZaliczeniowy/Screens/AddComputer.xaml.cs
+++ ProjektZaliczeniowy/ProjektZaliczeniowy/Screens/AddComputer.xaml.cs
@@ -23,11 +23,58 @@ namespace ProjektZaliczeniowy.Screens
         public AddComputer()
         {
             InitializeComponent();
+            fillComputerTypes();
+        }
+
+        private void fillComputerTypes()
+        {
+            computerTypeSelect.Items.Clear();
+
+            computerTypeSelect.Items.Add("P");
+            computerTypeSelect.Items.Add("L");
+        }
+
+        private void fillComputerModels()
+        {
+            computerModelSelect.Items.Clear();
+
+            computerModelSelect.DisplayMemberPath = "Text";
+            computerModelSelect.SelectedValuePath = "Value";
+
+            if (computerTypeSelect.SelectedValue != null)
+            {
+                var selectedType = computerTypeSelect.SelectedValue.ToString();
+                var db = new computerEntities();
+                var computerModels = db.Models.Where(m => m.ModelType == selectedType).ToList();
+                foreach(var computerModel in computerModels)
+                {
+                    computerModelSelect.Items.Add(new { Text = computerModel.ModelName, Value = computerModel.ModelID });
+                }
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            var db = new computerEntities();
 
+            var computerModelId = int.Parse(computerModelSelect.SelectedValue.ToString());
+            var computerType = computerTypeSelect.SelectedValue.ToString();
+            var computerPrice = decimal.Parse(computerPriceInput.Text);
+            var computerName = computerNameInput.Text;
+
+            var newComputer = new Computers() {
+                ComputerModelId = computerModelId,
+                ComputerName = computerName,
+                ComputerPrice = computerPrice,
+                ComputerType = computerType
+            };
+
+            db.Computers.Add(newComputer);
+        }
+
+        private void computerTypeSelect_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            fillComputerModels();
         }
     }
 }
